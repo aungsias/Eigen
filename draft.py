a6be78a5c822b4fe2a5c4ee532480a35f4b69bc7
aungsias <106785278+aungsias@users.noreply.github.com>
2023-09-23 17:23:38 -0700

    Updating draft.py

--- draft.py
+++ draft.py
@@ -8,58 +8,70 @@ from io import BytesIO
 prices = pd.read_csv("workflow/data/snp_prices.csv", index_col=0, parse_dates=True)
 available_tickers = prices.columns.to_list()
 
-# Title
-st.title("Stock Price Dashboard")
+page = st.sidebar.selectbox(
+    'Choose a page',
+    ('View Stocks', 'Our Services')
+)
 
-# User input
-ticker = st.selectbox("Choose Stock Ticker:", available_tickers, index=0)
-start_date = st.date_input("Start Date:", value=dt.datetime(2015, 1, 1))
-end_date = st.date_input("End Date:", value=dt.datetime.now())
 
-# Fetch stock data from the CSV
-stock_data = prices.loc[(prices.index >= pd.Timestamp(start_date)) & (prices.index <= pd.Timestamp(end_date)), [ticker]]
+if page == 'View Stocks':
+    st.title("Stock Price Dashboard")
 
-if stock_data.empty:
-    st.write("No data available for the given dates.")
-else:
-    # Option to display table
-    show_table = st.checkbox("See Table")
-    if show_table:
-        st.dataframe(stock_data)
-    
-    # Download CSV button
-    csv_buffer = BytesIO()
-    stock_data.to_csv(csv_buffer)
-    csv_buffer.seek(0)
-    st.download_button(
-        label="Download CSV",
-        data=csv_buffer,
-        file_name=f"{ticker}_{start_date}_{end_date}.csv",
-        mime="text/csv",
-    )
+    # User input
+    ticker = st.selectbox("Choose Stock Ticker:", available_tickers, index=0)
+    start_date = st.date_input("Start Date:", value=dt.datetime(2015, 1, 1))
+    end_date = st.date_input("End Date:", value=dt.datetime.now())
 
-    # Plotting with Plotly
-    fig = go.Figure()
+    # Fetch stock data from the CSV
+    stock_data = prices.loc[(prices.index >= pd.Timestamp(start_date)) & (prices.index <= pd.Timestamp(end_date)), [ticker]]
 
-    # Add traces
-    fig.add_trace(
-        go.Scatter(
-            x=stock_data.index,
-            y=stock_data[ticker],
-            mode='lines',
-            name='Closing Price',
-            line=dict(color='green')
+
+    if stock_data.empty:
+        st.write("No data available for the given dates.")
+    else:
+        # Option to display table
+        show_table = st.checkbox("See Table")
+        if show_table:
+            st.dataframe(stock_data)
+        
+        # Download CSV button
+        csv_buffer = BytesIO()
+        stock_data.to_csv(csv_buffer)
+        csv_buffer.seek(0)
+        st.download_button(
+            label="Download CSV",
+            data=csv_buffer,
+            file_name=f"{ticker}_{start_date}_{end_date}.csv",
+            mime="text/csv",
         )
-    )
 
-    # Customize layout
-    fig.update_layout(
-        title=f"{ticker} Closing Prices",
-        xaxis_title="Date",
-        yaxis_title="Closing Price (USD)",
-        template="plotly_dark",
-        xaxis=dict(showline=True, showgrid=False, showticklabels=True, linecolor='rgb(204, 204, 204)', linewidth=2),
-        yaxis=dict(showgrid=False, zeroline=False, showline=True, linecolor='rgb(204, 204, 204)'),
-    )
+        # Plotting with Plotly
+        fig = go.Figure()
 
-    st.plotly_chart(fig)
+        # Add traces
+        fig.add_trace(
+            go.Scatter(
+                x=stock_data.index,
+                y=stock_data[ticker],
+                mode='lines',
+                name='Closing Price',
+                line=dict(color='green')
+            )
+        )
+
+        # Customize layout
+        fig.update_layout(
+            title=f"{ticker} Closing Prices",
+            xaxis_title="Date",
+            yaxis_title="Closing Price (USD)",
+            template="plotly_dark",
+            xaxis=dict(showline=True, showgrid=False, showticklabels=True, linecolor='rgb(204, 204, 204)', linewidth=2),
+            yaxis=dict(showgrid=False, zeroline=False, showline=True, linecolor='rgb(204, 204, 204)'),
+        )
+
+        st.plotly_chart(fig)
+
+# Code for "Our Services" page
+elif page == 'Our Services':
+    st.title("Our Services")
+    st.write("Information about our services.")
